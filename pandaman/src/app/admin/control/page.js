80ba7dc2668d5e387aab54230e304604df15637f
mheepandaaa <yongthanee@gmail.com>
2024-02-10 14:20:58 +0700

    admin_mockup

--- pandaman/src/app/admin/control/page.js
+++ pandaman/src/app/admin/control/page.js
@@ -30,12 +30,29 @@ export default function control() {
   };
     return (
       <main className="flex flex-col bg-white">
-        <div className='flex flex-row p-4 items-center'>
-          <h1 className='font-bold'>พี่ช้าง อาหารตามสั่ง</h1>
+        <div className='flex flex-row h-12 px-4 py-2 items-center justify-between shadow-md'>
+          <div className='flex items-center'>
+          <h2 className='font-bold'>พี่ช้าง อาหารตามสั่ง</h2>
           <input type="checkbox" className="toggle toggle-error ml-4" />
-        <p className='ml-2'>ปิด/เปิดร้าน</p>
+          <p className='ml-2'>ปิด/เปิดร้าน</p>
+          </div>
+
+        <div className='flex items-center'>
+          <button className=" btn btn-xs text-white font-bold bg-gray-600 mr-2" onClick={cfgData}> แก้ไข</button>
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn-sm flex p-0 items-center">
+              <img className='w-auto' src='/account.svg'></img>
+            </div>
+             <ul tabIndex={0} className="dropdown-content z-[1] menu shadow bg-base-100 rounded-box w-36">
+                <li><a>ข้อมูล</a></li>
+                <li><a>ออกจากระบบ</a></li>
+             </ul>
+           </div>
         </div>
-        <div className='mt-8 flex flex-row items-center justify-between mx-16'>
+        </div>
+
+
+        {/* <div className='mt-8 flex flex-row items-center justify-between mx-16'>
         <div className='flex flex-col'>
         <div className='triangle bg-coral mx-8 mt-8' onClick={() => sendMessage('up')}></div>
         <div className="triangle2 bg-coral mx-8 mt-16" onClick={() => sendMessage('down')}></div>
@@ -43,11 +60,117 @@ export default function control() {
         <button className="btn w-40 h-20 text-white font-bold bg-coral drop-shadow-md border-none" onClick={() => sendMessage('accept')}> <h2>ยืนยันออเดอร์</h2></button>
         <button className="btn w-40 h-20 text-white font-bold bg-green-600 drop-shadow-md border-none" onClick={() => sendMessage('finish')}> <h2>ออเดอร์เสร็จ</h2></button>
         <button className="btn w-40 h-20 text-white font-bold bg-red-600 drop-shadow-md border-none" onClick={() => sendMessage('cancel')}> <h2>ลบออเดอร์</h2></button>
-        </div>
-        <div className='absolute bottom-0 right-0'>
-          <button className=" btn btn-sm text-white font-bold bg-gray-600 mr-4 mb-4" onClick={cfgData}> แก้ไข</button>
-          <button className=" btn btn-sm text-white font-bold bg-gray-600 mr-4 mb-4" onClick={showData}> ข้อมูล</button>
-        </div>
+        </div> */}
+
+<div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>ออเดอร์</th>
+        <th>เมนู</th>
+        <th>เพิ่มเติม</th>
+        <th>ออเดอร์รวม</th>
+        <th>สถานะ</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>1</th>
+        <td>กระเพรา หมู <br></br>ผัดผงกะหรี่ กุ้ง</td>
+        <td>ไข่ดาว 2 , พิเศษ 3 <br></br>ไข่ดาว 1 , พิเศษ 2</td>
+        <td>4</td>
+        <td className='flex justify-center'>
+          <div className='flex-col'>
+          <div className="dropdown dropdown-end">
+            <button tabIndex={0} role="button" className="btn btn-sm justify-center text-white font-bold bg-black pr-1">
+              <div className='flex items-center'>
+               กำลังทำ 
+               <img className='w-auto h-7 justify-end' src='/dropdown.svg'></img>
+              </div>
+            </button>
+             <ul tabIndex={0} className="dropdown-content z-[1] menu shadow bg-base-100 rounded-box w-36">
+                <li><a>ออเดอร์เสร็จ</a></li>
+             </ul>
+           </div>
+            <br></br>
+            <div className="dropdown dropdown-end">
+            <button tabIndex={0} role="button" className="btn btn-sm justify-center text-white font-bold bg-black pr-1">
+              <div className='flex items-center'>
+               กำลังทำ 
+               <img className='w-auto h-7 justify-end' src='/dropdown.svg'></img>
+              </div>
+            </button>
+             <ul tabIndex={0} className="dropdown-content z-[1] menu shadow bg-base-100 rounded-box w-36">
+                <li><a>ออเดอร์เสร็จ</a></li>
+             </ul>
+           </div>
+          </div>
+          <div className='flex'>
+          <div className="dropdown dropdown-end">
+          <button className="btn btn-sm text-white font-bold bg-black h-full py-2 px-0">
+              <img className='w-auto h-7' src='/dropdown.svg'></img>
+            </button>
+             <ul tabIndex={0} className="dropdown-content z-[1] menu shadow bg-base-100 rounded-box w-36">
+                <li><a>ออเดอร์เสร็จ</a></li>
+             </ul>
+           </div>
+          </div>
+        </td>
+      </tr>
+      {/* row 2 */}
+      <tr>
+        <th>2</th>
+        <td>ข้าวผัด หมูกรอบ</td>
+        <td>พิเศษ 2</td>
+        <td>3</td>
+        <td>
+        <div className="dropdown dropdown-end">
+        <button tabIndex={0} role="button" className="btn btn-sm justify-center text-white font-bold bg-green-600 pr-1">
+              <div className='flex items-center'>
+               เสร็จแล้ว 
+               <img className='w-auto h-7 justify-end' src='/dropdown.svg'></img>
+              </div>
+            </button>
+             <ul tabIndex={0} className="dropdown-content z-[1] menu shadow bg-base-100 rounded-box w-36">
+                <li><a>ลบรายการ</a></li>
+             </ul>
+           </div>
+        </td>
+      </tr>
+      {/* row 3 */}
+      <tr>
+      <th>3</th>
+        <td>ข้าวผัด หมูกรอบ</td>
+        <td>พิเศษ 2</td>
+        <td>3</td>
+        <td><button className=" btn btn-sm text-blue-900 font-bold mr-2"> รอการชำระเงิน</button></td>
+      </tr>
+      {/* row 4 */}
+      <tr>
+      <th>4</th>
+      <td>กระเพรา หมู <br></br>ผัดผงกะหรี่ กุ้ง</td>
+        <td>ไข่ดาว 2 , พิเศษ 3 <br></br>ไข่ดาว 1 , พิเศษ 2</td>
+        <td>3</td>
+        <td>
+          <div className="dropdown dropdown-end">
+            <button tabIndex={0} role="button" className="btn btn-sm justify-center text-white font-bold bg-red-500 pr-1">
+              <div className='flex items-center'>
+              รอการยืนยัน 
+               <img className='w-auto h-7 justify-end' src='/dropdown.svg'></img>
+              </div>
+            </button>
+             <ul tabIndex={0} className="dropdown-content z-[1] menu shadow bg-base-100 rounded-box w-36">
+                <li><a>ยืนยัน</a></li>
+                <li><a>ยกเลิก</a></li>
+             </ul>
+           </div>
+          </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 
         <dialog id="dataModal" className="modal">
   <div className="modal-box bg-white flex flex-col p-0 w-full h-3/4">
